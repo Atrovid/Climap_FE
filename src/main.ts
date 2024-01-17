d89f33af35c78fa946abeaf104e9d4ef30803a1d
Fheuef <fheuef27@gmail.com>
2024-01-17 09:30:31 +0100

    Presentation first draft

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,37 @@
-import { setupCounter } from "./counter.ts";
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
 import { html } from "./util.ts";
-import viteLogo from "/vite.svg";
+
+const climapLogo = "/clipmap.png";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = html`
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="${viteLogo}" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://www.typescriptlang.org/" target="_blank">
-			<img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-		</a>
-		<h1>Vite + TypeScript</h1>
-		<div class="card">
-			<button id="counter" type="button"></button>
+	<div class="section-bg-primary">
+		<img src="${climapLogo}" class="main-logo" />
+		<h2>Capteur intelligent, conditions idéales garanties</h2>
+	</div>
+	<div class="section-bg-secondary">
+		<div class="section-content">
+			<p class="center-text ">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+				ex ea commodo consequat. [Inserer texte “Et si”]
+			</p>
 		</div>
-		<p class="read-the-docs">Click on the Vite and TypeScript logos to learn more</p>
+		<div class="section-content">
+			<div class="content-flex-h">
+				<img src="map_example.png" class="map-img" />
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. [Inserer texte “imaginez”]
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="section-bg-primary">
+		<div class="section-content">
+			<h2>Capteur</h2>
+		</div>
 	</div>
 `;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
