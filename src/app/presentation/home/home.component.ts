df77cf3049b39fa3e5b81cfea30ec5da05bbf237
lmoreau <lorie.moreau@ecole.ensicaen.fr>
2024-01-18 21:40:29 +0100

    end of the day

--- src/app/presentation/home/home.component.ts
+++ src/app/presentation/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SectionComponent } from '../common/section/section.component';
 import { CriticComponent } from "../critic/critic.component";
+import { FormulaireComponent } from '../formulaire/formulaire.component';
 import { MarketComponent } from "../market/market.component";
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { TechniqueComponent } from "../technique/technique.component";
@@ -11,7 +12,7 @@ import { TechniqueComponent } from "../technique/technique.component";
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [SectionComponent, MarketComponent, RouterLink, NavBarComponent, CriticComponent, TechniqueComponent]
+    imports: [SectionComponent, MarketComponent, RouterLink, NavBarComponent, CriticComponent, TechniqueComponent, FormulaireComponent]
 })
 export class HomeComponent {
   // constructor(){}
