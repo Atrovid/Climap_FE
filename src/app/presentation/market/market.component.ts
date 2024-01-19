26407e886026fa9b78ca72599079a55aabf74397
Paul Morin <paul.morin@ecole.ensicaen.fr>
2024-01-19 10:36:22 +0100

    asset links

--- src/app/presentation/market/market.component.ts
+++ src/app/presentation/market/market.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 
 @Component({
-    selector: 'app-market',
+    selector: "app-market",
     standalone: true,
-    templateUrl: './market.component.html',
-    styleUrl: './market.component.css',
-    imports: [NavBarComponent]
+    templateUrl: "./market.component.html",
+    styleUrl: "./market.component.css",
+    imports: [NavBarComponent, RouterModule],
 })
 export class MarketComponent {
-  constructor(private router: Router) {}
-  
-  navigateOnMap() {
-    this.router.navigate(['/', 'map']);
-  }
+    constructor(private router: Router) {}
+
+    navigateOnMap() {
+        this.router.navigate(["/", "map"]);
+    }
 }
